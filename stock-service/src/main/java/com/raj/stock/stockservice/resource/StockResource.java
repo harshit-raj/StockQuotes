f17d600ef2b270e44ba6cc1cd30ea79a33884b02
Harshit Raj <raj.0489@gmail.com>
2018-06-11 16:59:28 -0400

    stockservice start

---
+++ stock-service/src/main/java/com/raj/stock/stockservice/resource/StockResource.java
@@ -0,0 +1,27 @@
+package com.raj.stock.stockservice.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import yahoofinance.quotes.stock.StockQuote;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rest/stock")
+public class StockResource {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/{username}")
+    public List<StockQuote> getStock(@PathVariable("username") final String userName){
+        List<String> quotes = restTemplate.getForObject("http://localhost:8300/rest/db"+userName,List.class);
+
+        return null;
+
+    }
+}
